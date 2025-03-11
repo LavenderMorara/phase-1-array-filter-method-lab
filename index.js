f70819478594c3c56f8a3a71ef0fefebc5664c83
LavenderMorara <chiyamorara@gmail.com>
2025-03-11 14:35:54 +0300

    did filter()

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
-
-function findMatching(array,name){
-let filterArray=array.filter(driver => driver.toLowerCase() === name.toLowerCase());
-return filterArray
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+function findMatching(arr,name){
+    let filteredArray= arr.filter((driver)=>{return driver.toLowerCase()=== name.toLowerCase()});
+    return filteredArray;
 }
 console.log(findMatching(drivers,'Bobby'))
-console.log(findMatching(drivers,'Sammy'))
-console.log(findMatching(drivers,'Benda'))
 
-function fuzzyMatch(array, letter){
-    let filterArray=array.filter(driver=>driver.startsWith(letter))
-    return filterArray
+function fuzzyMatch(arr,letter){
+let filteredArray=arr.filter((driver)=>{return driver.startsWith(letter)})
+return filteredArray
 }
 console.log(fuzzyMatch(drivers,'Sa'))
 
@@ -31,8 +28,12 @@ const Drivers = [
       name: 'Bobby',
       hometown: 'Tampa Bay' }
   ];
-function matchName(array,name){
-    let filterArray=array.filter(driver=>driver.name===name) //driver.name ensures only the element with the given name property is returned
-    return filterArray 
+//   function matchName(arr,n){
+//     let filteredArray=arr.filter((driver)=>{driver.name===n})
+//     return filteredArray
+// }
+function matchName(arr, n) {
+    return arr.filter(driver => driver.name === n);
 }
-console.log(matchName(Drivers,'Bobby'))
+
+console.log(Drivers,'Sally')
